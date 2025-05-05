51cb522f6c7e6839f60f280bcd4f50094b236572
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-05-05 14:15:47 +0500

    fix: turn off pulseaudio

--- modules/nixos/pipewire/default.nix
+++ modules/nixos/pipewire/default.nix
@@ -17,7 +17,7 @@ in {
   };
 
   config = mkIf cfg.enable {
-    services.pulseaudio.enable = false;
+    # services.pulseaudio.enable = false;
     security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
