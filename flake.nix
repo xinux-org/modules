71be03e31ad4316c92a2df86617261d92b7b8ce4
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-05-05 14:15:34 +0500

    chore: pin to 24.11 stable branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Xinux Modules";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
     # Xinux provided
 
