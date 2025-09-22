aaa853ce10f10f3c097977b7bce1808c9a0d0b86
bemeritus <bemerituss@gmail.com>
2025-09-22 17:16:30 +0500

    chore: added custom grub theme

--- modules/nixos/biosboot/default.nix
+++ modules/nixos/biosboot/default.nix
@@ -11,8 +11,22 @@ in {
   config = {
     boot.loader = {
       systemd-boot.enable = false;
-      grub.enable = true;
-      grub.useOSProber = true;
+      grub = {
+        enable = true;
+        devices = ["nodev"];
+        useOSProber = true;
+        theme = pkgs.stdenv.mkDerivation {
+          pname = "bootloader-theme";
+          version = "1.0.2";
+          src = pkgs.fetchFromGitHub {
+            owner = "xinux-org";
+            repo = "bootloader-theme";
+            tag = "v1.0.2";
+            hash = "sha256-uEZfnhmXpjgmikppTlM8OQ5FAsmFdvi8dwIAZtaQ/MQ=";
+          };
+          installPhase = "cp -r $src/nixos $out";
+        };
+      };
     };
     boot.tmp.cleanOnBoot = mkDefault true;
   };
