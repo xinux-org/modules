1bc3ccff19bd73f39403dfd0c4d5bce85f134f61
bahrom04 <magdiyevbahrom@gmail.com>
2025-11-04 21:45:02 +0500

    fix: attr set merged to set-2

--- modules/nixos/xinux/default.nix
+++ modules/nixos/xinux/default.nix
@@ -9,25 +9,18 @@
 with lib; let
   cfg = config.modules.xinux;
 in {
-  imports =
-    (
-      if config.modules.gnome.enable
-      then [
-        ./gnome.nix
-      ]
-      else []
-    )
-    ++ [
-      ./graphical.nix
-      ./hardware.nix
-      ./version.nix
-    ];
-    # [mkIf config.modules.gnome.enable [./gnome.nix]]
-    # ++ [
-    #   ./graphical.nix
-    #   ./hardware.nix
-    #   ./version.nix
-    # ];
+  imports = [
+    ./graphical.nix
+    ./hardware.nix
+    ./version.nix
+    ./gnome.nix
+  ];
+  # [mkIf config.modules.gnome.enable [./gnome.nix]]
+  # ++ [
+  #   ./graphical.nix
+  #   ./hardware.nix
+  #   ./version.nix
+  # ];
 
   options.modules.xinux = with types; {
     nixSoftwareCenter.enable = mkOption {
