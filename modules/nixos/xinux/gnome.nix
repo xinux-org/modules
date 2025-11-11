9b7d70de57fd58f0fc8918512a51184313856cb8
bahrom04 <magdiyevbahrom@gmail.com>
2025-11-11 16:29:50 +0500

    chore: add removing extensions in minimal mode

--- modules/nixos/xinux/gnome.nix
+++ modules/nixos/xinux/gnome.nix
@@ -29,116 +29,118 @@ in {
     enable = lib.mkEnableOption "Xinux GNOME configuration";
   };
 
-  config =
-    lib.mkIf config.xinux.gnome.enable {
-      xinux.graphical.enable = true;
-      services.desktopManager.gnome = {
-        favoriteAppsOverride = lib.mkDefault ''
-          [org.gnome.shell]
-          favorite-apps=[ 'firefox.desktop', 'org.gnome.Geary.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.Nautilus.desktop', 'org.xinux.NixSoftwareCenter.desktop' ]
-        '';
-        extraGSettingsOverrides = ''
-          [org.gnome.desktop.background]
-          picture-uri='file://${pkgs.nixos-artwork.wallpapers.nineish.gnomeFilePath}'
-          picture-uri-dark='file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}'
+  config = lib.mkIf config.xinux.gnome.enable {
+    xinux.graphical.enable = true;
+    services.desktopManager.gnome = {
+      favoriteAppsOverride = lib.mkDefault ''
+        [org.gnome.shell]
+        favorite-apps=[ 'firefox.desktop', 'org.gnome.Geary.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.Nautilus.desktop', 'org.xinux.NixSoftwareCenter.desktop' ]
+      '';
+      extraGSettingsOverrides = ''
+        [org.gnome.desktop.background]
+        picture-uri='file://${pkgs.nixos-artwork.wallpapers.nineish.gnomeFilePath}'
+        picture-uri-dark='file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}'
 
-          [org.gnome.desktop.screensaver]
-          picture-uri='file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}'
+        [org.gnome.desktop.screensaver]
+        picture-uri='file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}'
 
-          [org.gnome.desktop.interface]
-          color-scheme='prefer-dark'
+        [org.gnome.desktop.interface]
+        color-scheme='prefer-dark'
 
-          [org.gnome.shell]
-          disable-user-extensions=false
+        [org.gnome.shell]
+        disable-user-extensions=false
 
-          [org.gnome.shell]
-          enabled-extensions=['user-theme@gnome-shell-extensions.gcampax.github.com', 'dash-to-dock@micxgx.gmail.com', 'appindicatorsupport@rgcjonas.gmail.com', 'light-style@gnome-shell-extensions.gcampax.github.com', 'system-monitor@gnome-shell-extensions.gcampax.github.com', 'clipboard-indicator@tudmotu.com']
+        [org.gnome.shell]
+        enabled-extensions=['user-theme@gnome-shell-extensions.gcampax.github.com', 'dash-to-dock@micxgx.gmail.com', 'appindicatorsupport@rgcjonas.gmail.com', 'light-style@gnome-shell-extensions.gcampax.github.com', 'system-monitor@gnome-shell-extensions.gcampax.github.com', 'clipboard-indicator@tudmotu.com']
 
-          [org.gnome.mutter]
-          dynamic-workspaces=true
+        [org.gnome.mutter]
+        dynamic-workspaces=true
 
-          [org.gnome.mutter]
-          edge-tiling=true
+        [org.gnome.mutter]
+        edge-tiling=true
 
-          [org.gnome.desktop.interface]
-          icon-theme='Papirus-Dark'
+        [org.gnome.desktop.interface]
+        icon-theme='Papirus-Dark'
 
-          [org.gnome.desktop.interface]
-          color-scheme='default'
+        [org.gnome.desktop.interface]
+        color-scheme='default'
 
-          [org.gnome.desktop.datetime]
-          automatic-timezone=true
+        [org.gnome.desktop.datetime]
+        automatic-timezone=true
 
-          [org.gnome.tweaks]
-          show-extensions-notice=false
+        [org.gnome.tweaks]
+        show-extensions-notice=false
 
-          [org.gnome.desktop.wm.preferences]
-          button-layout='appmenu:minimize,maximize,close'
+        [org.gnome.desktop.wm.preferences]
+        button-layout='appmenu:minimize,maximize,close'
 
-          [org.gnome.desktop.interface]
-          monospace-font-name='JetBrainsMono Nerd Font 10'
+        [org.gnome.desktop.interface]
+        monospace-font-name='JetBrainsMono Nerd Font 10'
 
-          [org.gnome.shell.extensions.dash-to-dock]
-          multi-monitor=true
+        [org.gnome.shell.extensions.dash-to-dock]
+        multi-monitor=true
 
-          [org.gnome.shell.extensions.dash-to-dock]
-          apply-custom-theme=true
-        '';
-        extraGSettingsOverridePackages = [
-          pkgs.gsettings-desktop-schemas
-          pkgs.gnome-shell
-        ];
-      };
-
-      # Setting daemons
-      services = {
-        # Udev daemon management
-        udev.packages = with pkgs; [gnome-settings-daemon];
-      };
-
-      programs = {
-        gnupg.agent = {
-          enable = true;
-          enableSSHSupport = true;
-        };
-        # Enabling seahorse keyring
-        seahorse = {
-          enable = true;
-        };
-      };
-
-      fonts.packages = with pkgs; [
-        noto-fonts
-        noto-fonts-cjk-sans
-        noto-fonts-color-emoji
-        liberation_ttf
-        fira-code
-        fira-code-symbols
-        nerd-fonts.jetbrains-mono
+        [org.gnome.shell.extensions.dash-to-dock]
+        apply-custom-theme=true
+      '';
+      extraGSettingsOverridePackages = [
+        pkgs.gsettings-desktop-schemas
+        pkgs.gnome-shell
       ];
+    };
 
-      environment.variables = {
-        # Disable compositing mode in WebKitGTK
-        # https://github.com/NixOS/nixpkgs/issues/32580
-        WEBKIT_DISABLE_COMPOSITING_MODE = 1;
+    # Setting daemons
+    services = {
+      # Udev daemon management
+      udev.packages = with pkgs; [gnome-settings-daemon];
+    };
+
+    programs = {
+      gnupg.agent = {
+        enable = true;
+        enableSSHSupport = true;
       };
+      # Enabling seahorse keyring
+      seahorse = {
+        enable = true;
+      };
+    };
 
-      services.xserver.excludePackages = [pkgs.xterm];
+    fonts.packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-color-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      nerd-fonts.jetbrains-mono
+    ];
 
-      environment.gnome.excludePackages = [
-        pkgs.xterm
-        nixos-background-info
-      ];
+    environment.variables = {
+      # Disable compositing mode in WebKitGTK
+      # https://github.com/NixOS/nixpkgs/issues/32580
+      WEBKIT_DISABLE_COMPOSITING_MODE = 1;
+    };
 
-      environment.systemPackages = [
+    services.xserver.excludePackages = [pkgs.xterm];
+
+    environment.gnome.excludePackages = [
+      pkgs.xterm
+      nixos-background-info
+    ];
+
+    environment.systemPackages =
+      # if minimal mode enabled keep these extensions
+      lib.mkIf (!config.modules.gnome.removeUtils.enable) [
         xinux-background-info
 
         pkgs.gnomeExtensions.appindicator
         pkgs.gnomeExtensions.dash-to-dock
+        pkgs.papirus-icon-theme
+      ]
+      ++ [
         pkgs.gnomeExtensions.gsconnect
         pkgs.gnomeExtensions.clipboard-indicator
-
-        pkgs.papirus-icon-theme
       ];
-    };
+  };
 }
