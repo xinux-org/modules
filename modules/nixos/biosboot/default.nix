81fd5eefa3af26578aa857da4f71d4038b59e85c
bahrom04 <magdiyevbahrom@gmail.com>
2025-11-20 15:36:23 +0500

    chore: set plymouth only for efiboot

--- modules/nixos/biosboot/default.nix
+++ modules/nixos/biosboot/default.nix
@@ -26,11 +26,6 @@ in {
           }/xinux";
         };
       };
-      # plymouth = {
-      #   enable = true;
-      #   theme = "mac-style";
-      #   themePackages = [pkgs.mac-style-plymouth];
-      # };
     };
     boot.tmp.cleanOnBoot = mkDefault true;
   };
