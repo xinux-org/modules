432f44c779fb75a3b96f48573a4124bee6ac65a3
BeMeritus <bemerituss@gmail.com>
2025-12-03 17:35:02 +0500

    chore: deleted shitty lines

--- modules/nixos/gnome/default.nix
+++ modules/nixos/gnome/default.nix
@@ -8,30 +8,6 @@
 with lib; let
   cfg = config.modules.gnome;
 in {
-  };
-
-  config = mkMerge [
-    {
-      # Enable the GNOME Desktop Environment.
-      services.displayManager.gdm.enable = true;
-      services.desktopManager.gnome.enable = true;
-      services.xserver.enable = true;
-  };
-
-  config = mkMerge [
-    {
-      # Enable the GNOME Desktop Environment.
-      services.displayManager.gdm.enable = true;
-      services.desktopManager.gnome.enable = true;
-      services.xserver.enable = true;
-  };
-
-  config = mkMerge [
-    {
-      # Enable the GNOME Desktop Environment.
-      services.displayManager.gdm.enable = true;
-      services.desktopManager.gnome.enable = true;
-      services.xserver.enable = true;
   options.modules.gnome = with types; {
     gsconnect.enable =
       mkEnableOption "Enable KDE Connect integration";
