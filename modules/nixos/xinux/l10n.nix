fbc3d946ecacb7c09dab3cea21535364e49feef8
bahrom04 <116780481+bahrom04@users.noreply.github.com>
2026-01-19 12:26:45 +0500

    Update modules/nixos/xinux/l10n.nix

--- modules/nixos/xinux/l10n.nix
+++ modules/nixos/xinux/l10n.nix
@@ -5,26 +5,31 @@ in {
     enable = true;
 
     # add uzbek keyboard
-    xkb.extraLayouts = {
-      uz = {
-        description = "Uzbek";
-        languages = ["uzb"];
-        symbolsFile = "${xkbPath}/uz";
-      };
-      uz-us = {
-        description = "Uzbek (US)";
-        languages = ["uzb"];
-        symbolsFile = "${xkbPath}/uz_us";
-      };
-      uz-2023 = {
-        description = "Uzbek (2023)";
-        languages = ["uzb"];
-        symbolsFile = "${xkbPath}/uz_2023";
-      };
-      uz-cyrillic = {
-        description = "Uzbek (Cyrillic)";
-        languages = ["uzb"];
-        symbolsFile = "${xkbPath}/uz_cyrillic";
+    xkb = {
+      variant = "latin";
+      layout = "uz,uz(latin),us,ru";
+
+      extraLayouts = {
+        uz = {
+          description = "Uzbek";
+          languages = ["uzb"];
+          symbolsFile = "${xkbPath}/uz";
+        };
+        uz-us = {
+          description = "Uzbek (US)";
+          languages = ["uzb"];
+          symbolsFile = "${xkbPath}/uz_us";
+        };
+        uz-2023 = {
+          description = "Uzbek (2023)";
+          languages = ["uzb"];
+          symbolsFile = "${xkbPath}/uz_2023";
+        };
+        uz-cyrillic = {
+          description = "Uzbek (Cyrillic)";
+          languages = ["uzb"];
+          symbolsFile = "${xkbPath}/uz_cyrillic";
+        };
       };
     };
 
