eeabaf161729620a586d9c36342cd1f4fe8dfe4f
bahrom04 <magdiyevbahrom@gmail.com>
2026-02-05 20:21:07 +0500

    feat: added system config check

---
+++ checks/xinux/configuration.nix
@@ -0,0 +1,39 @@
+{
+  pkgs,
+  ...
+}: {
+  networking.hostName = "xinux";
+
+  # Select internationalisation properties.
+  modules.xinux.language = "uz_UZ.UTF-8";
+
+  # Set the keyboard layout.
+  services.xserver.xkb = {
+    layout = "uz";
+    variant = "latin";
+  };
+  console.useXkbConfig = true;
+
+  users.users."a" = {
+    isNormalUser = true;
+    description = "a";
+    extraGroups = ["wheel" "networkmanager" "dialout"];
+  };
+
+  # Allow unfree packages
+  environment.sessionVariables.NIXPKGS_ALLOW_UNFREE = "1";
+
+  # List packages installed in system profile.
+  environment.systemPackages = with pkgs; [
+    htop
+  ];
+
+  system.stateVersion = "26.05";
+
+  # programs.nix-data = {
+  #   enable = true;
+  #   systemconfig = "/etc/nixos/systems/x86_64-linux/xinux/default.nix";
+  #   flake = "/etc/nixos/flake.nix";
+  #   flakearg = "xinux";
+  # };
+}
---
+++ checks/xinux/default.nix
@@ -0,0 +1,43 @@
+{
+  inputs,
+  pkgs,
+  ...
+}:
+pkgs.testers.runNixOSTest {
+  name = "tests that the base image boots";
+  nodes.machine = {...}: {
+    imports = with inputs.self; [
+      nixosModules.efiboot
+      nixosModules.gnome
+      nixosModules.kernel
+      nixosModules.networking
+      nixosModules.pipewire
+      nixosModules.printing
+      nixosModules.xinux
+      nixosModules.metadata
+      ./configuration.nix
+    ];
+
+    # virtually test nixosConfiguration. 
+    # I think we do not need this
+    # fileSystems."/" = {
+    #   # note this should be dynamic based on your disk
+    #   device = "/dev/sdb3";
+    #   fsType = "ext4";
+    # };
+  };
+
+  node = {
+    # since we are using an overlay, we must make pkgs writable
+    pkgsReadOnly = false;
+
+    specialArgs = {inherit inputs;};
+  };
+
+  testScript = ''
+    machine.start()
+    machine.wait_for_unit("multi-user.target")
+    machine.succeed("uname -a")
+    machine.succeed("echo Modules succesfully tested")
+  '';
+}
