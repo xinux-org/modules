decd2317b623377535a3157da6600288379c446b
bahrom04 <magdiyevbahrom@gmail.com>
2026-04-15 13:56:48 +0500

    chore: more fonts added

--- modules/nixos/xinux/gnome.nix
+++ modules/nixos/xinux/gnome.nix
@@ -94,13 +94,20 @@ in
     };
 
     fonts.packages = with pkgs; [
+      # An alternative popular Chinese font
+      wqy_zenhei
+
+      corefonts
+      vista-fonts-chs
       noto-fonts
-      noto-fonts-cjk-sans
       noto-fonts-color-emoji
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      nerd-fonts.jetbrains-mono
+      mplus-outline-fonts.osdnRelease
       liberation_ttf
       fira-code
       fira-code-symbols
-      nerd-fonts.jetbrains-mono
     ];
 
     environment.variables = {
