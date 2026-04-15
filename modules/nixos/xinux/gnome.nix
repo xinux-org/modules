b76ee93b9565533e5d654bf59206b5bd3f0c9006
bahrom04 <magdiyevbahrom@gmail.com>
2026-04-15 15:55:54 +0500

    chore: more fonts added

--- modules/nixos/xinux/gnome.nix
+++ modules/nixos/xinux/gnome.nix
@@ -93,22 +93,44 @@ in
       };
     };
 
-    fonts.packages = with pkgs; [
-      # An alternative popular Chinese font
-      wqy_zenhei
-
-      corefonts
-      vista-fonts-chs
-      noto-fonts
-      noto-fonts-color-emoji
-      noto-fonts-cjk-sans
-      noto-fonts-cjk-serif
-      nerd-fonts.jetbrains-mono
-      mplus-outline-fonts.osdnRelease
-      liberation_ttf
-      fira-code
-      fira-code-symbols
-    ];
+    fonts = {
+      enableDefaultPackages = true;
+      fontDir.enable = true;
+      packages = with pkgs; [
+        # An alternative popular Chinese font
+        wqy_zenhei
+        ubuntu-classic
+        corefonts
+        carlito
+        vista-fonts
+        vista-fonts-chs
+        font-bh-ttf
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
+        nerd-fonts.jetbrains-mono
+        mplus-outline-fonts.osdnRelease
+        fira-code
+        fira-code-symbols
+        hermit
+        source-code-pro
+        terminus_font
+        font-awesome
+        font-awesome_4
+        hack-font
+        noto-fonts
+        cantarell-fonts
+        powerline-fonts
+        roboto
+        roboto-slab
+        eb-garamond
+        fira-code
+        fira-code-symbols
+        mplus-outline-fonts.githubRelease
+        dina-font
+        proggyfonts
+      ];
+    };
 
     environment.variables = {
       # Disable compositing mode in WebKitGTK
