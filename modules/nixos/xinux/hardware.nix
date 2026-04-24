871ca3c3c107d64544e7ed255875a6f110708707
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-04-25 02:22:10 +0500

    feat: gpu driver support

--- modules/nixos/xinux/hardware.nix
+++ modules/nixos/xinux/hardware.nix
@@ -5,6 +5,7 @@
   ...
 }:
 let
+  cfg = config.xinux.graphical;
   nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
     export __NV_PRIME_RENDER_OFFLOAD=1
     export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
@@ -16,22 +17,21 @@ in
 {
   options.xinux.graphical = {
     enable = lib.mkEnableOption "Xinux default graphical configurations (not including DE)";
+
     gpu = lib.mkOption {
       type =
         with lib.types;
-        listOf enum [
+        listOf (enum [
           "intel"
           "nvidia"
           "amdgpu"
-        ];
+        ]);
+      default = [ ];
+      description = "Graphical driver to be added to the host system.";
     };
-
   };
 
   config = lib.mkMerge [
-    (lib.mkIf (config.hardware.nvidia.prime.offload.enable && config.xinux.graphical.enable) {
-      environment.systemPackages = [ nvidia-offload ];
-    })
     (lib.mkIf config.xinux.graphical.enable {
       # Enable fwupd
       services.fwupd.enable = lib.mkDefault true;
@@ -40,6 +40,61 @@ in
       hardware.graphics = {
         enable = lib.mkDefault true;
         enable32Bit = lib.mkDefault (config.hardware.graphics.enable && pkgs.stdenv.hostPlatform.isx86);
+      };
+    })
+
+    # if the list includes intel in the list
+    (lib.mkIf (builtins.elem "intel" cfg.gpu) {
+      # GPU (Intel)
+      graphics = {
+        enable = lib.mkDefault true;
+        extraPackages = with pkgs; [
+          vpl-gpu-rt
+          intel-media-driver
+          intel-ocl
+          intel-vaapi-driver
+        ];
+      };
+    })
+
+    # if the list includes nvidia in the list
+    (lib.mkIf (builtins.elem "nvidia" cfg.gpu) {
+      boot = {
+        kernelModules = [
+          "nvidia"
+          "nvidia_modeset"
+          "nvidia_uvm"
+          "nvidia_drm"
+        ];
+        kernelParams = [
+          "nvidia-drm.modeset=1"
+        ];
+      };
+      # List packages system hardware configuration
+      hardware = {
+        # GPU (Nvidia)
+        nvidia = {
+          modesetting.enable = true;
+          powerManagement.enable = true;
+          powerManagement.finegrained = false;
+          open = false;
+          nvidiaSettings = true;
+          package = config.boot.kernelPackages.nvidiaPackages.latest;
+        };
+      };
+    })
+
+    # NVIDIA offload support
+    (lib.mkIf (config.hardware.nvidia.prime.offload.enable && config.xinux.graphical.enable) {
+      environment.systemPackages = [ nvidia-offload ];
+    })
+
+    # if the list includes amdgpu in the list
+    (lib.mkIf (builtins.elem "amdgpu" cfg.gpu) {
+      hardware.amdgpu = {
+        initrd.enable = true;
+        opencl.enable = true;
+        zluda.enable = true;
       };
     })
   ];
