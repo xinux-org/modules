41546b9fce9ff2750eaed03d8149fbe8b7882088
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-04-28 21:07:51 +0500

    chore: gpu autodetection

--- modules/nixos/graphical/default.nix
+++ modules/nixos/graphical/default.nix
@@ -7,101 +7,54 @@
 with lib;
 let
   cfg = config.modules.graphical;
-
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec "$@"
-  '';
 in
 {
+  imports = [ ./gpu.nix ];
+
   options.modules.graphical = with types; {
     enable = mkEnableOption "Xinux default graphical configurations (not including DE)";
 
     provider = mkOption {
-      type = listOf (enum [
-        "intel"
-        "nvidia"
-        "amdgpu"
-        "modesetting"
-        "fbdev"
-      ]);
-      default = [
-        "modesetting"
-        "fbdev"
+      type = enum [
+        "default"
+
       ];
-      description = "Graphical driver to be added to the host system.";
+      default = "default";
+      description = "The graphics card provider to install driver for.";
     };
   };
 
   config = mkIf cfg.enable (mkMerge [
     {
-      # Enable fwupd
-      services.fwupd.enable = lib.mkDefault true;
-
-      # Add opengl/vulkan support
-      hardware.graphics = {
-        enable = lib.mkDefault true;
-        enable32Bit = lib.mkDefault (config.hardware.graphics.enable && pkgs.stdenv.hostPlatform.isx86);
-      };
-
-      services.xserver.videoDrivers = cfg.provider;
+      modules.gpu.enable = true;
     }
 
-    # if the list includes intel in the list
-    (lib.mkIf (builtins.elem "intel" cfg.provider) {
-      # GPU (Intel)
-      hardware.graphics = {
-        extraPackages = with pkgs; [
-          vpl-gpu-rt
-          intel-media-driver
-          intel-ocl
-          intel-vaapi-driver
-        ];
-      };
+    (lib.mkIf (cfg.provider == "default") {
+      modules.gpu.vendor = [
+        "modesetting"
+        "fbdev"
+      ];
     })
 
-    # if the list includes nvidia in the list
-    (lib.mkIf (builtins.elem "nvidia" cfg.provider) {
-      boot = {
-        kernelModules = [
-          "nvidia"
-          "nvidia_modeset"
-          "nvidia_uvm"
-          "nvidia_drm"
-        ];
-        kernelParams = [
-          "nvidia-drm.modeset=1"
-        ];
-      };
-      # List packages system hardware configuration
-      hardware = {
-        # GPU (Nvidia)
-        nvidia = {
-          modesetting.enable = true;
-          powerManagement.enable = true;
-          powerManagement.finegrained = false;
-          open = false;
-          nvidiaSettings = true;
-          package = config.boot.kernelPackages.nvidiaPackages.latest;
-        };
-      };
+    (lib.mkIf (cfg.provider == "intel") {
+      modules.gpu.vendor = [ "intel" ];
     })
 
-    # NVIDIA offload support
-    (lib.mkIf (config.hardware.nvidia.prime.offload.enable && cfg.enable) {
-      environment.systemPackages = [ nvidia-offload ];
+    (lib.mkIf (cfg.provider == "nvidia") {
+      # can be combined with amd or intel
+      modules.gpu.vendor = [
+        "nvidia"
+      ]
+      ++ (optionals config.hardware.cpu.amd.updateMicrocode [ "amdgpu" ])
+      ++ (optionals config.hardware.cpu.intel.updateMicrocode [ "intel" ]);
     })
 
-    # if the list includes amdgpu in the list
     (lib.mkIf (builtins.elem "amdgpu" cfg.provider) {
-      hardware.amdgpu = {
-        initrd.enable = true;
-        opencl.enable = true;
-        zluda.enable = true;
-      };
+      # can be combined with intel
+      modules.gpu.vendor = [
+        "amdgpu"
+      ]
+      ++ (optionals config.hardware.cpu.intel.updateMicrocode [ "intel" ]);
     })
   ]);
 }
---
+++ modules/nixos/graphical/gpu.nix
@@ -0,0 +1,107 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+with lib;
+let
+  cfg = config.modules.gpu;
+
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec "$@"
+  '';
+in
+{
+  options.modules.gpu = with types; {
+    enable = mkEnableOption "Xinux default graphical configurations (not including DE)";
+
+    vendor = mkOption {
+      type = listOf (enum [
+        "intel"
+        "nvidia"
+        "amdgpu"
+        "modesetting"
+        "fbdev"
+      ]);
+      default = [
+        "modesetting"
+        "fbdev"
+      ];
+      description = "Graphical driver to be added to the host system.";
+    };
+  };
+
+  config = mkIf cfg.enable (mkMerge [
+    {
+      # Enable fwupd
+      services.fwupd.enable = lib.mkDefault true;
+
+      # Add opengl/vulkan support
+      hardware.graphics = {
+        enable = lib.mkDefault true;
+        enable32Bit = lib.mkDefault (config.hardware.graphics.enable && pkgs.stdenv.hostPlatform.isx86);
+      };
+
+      services.xserver.videoDrivers = cfg.vendor;
+    }
+
+    # if the list includes intel in the list
+    (lib.mkIf (builtins.elem "intel" cfg.vendor) {
+      # GPU (Intel)
+      hardware.graphics = {
+        extraPackages = with pkgs; [
+          vpl-gpu-rt
+          intel-media-driver
+          intel-ocl
+          intel-vaapi-driver
+        ];
+      };
+    })
+
+    # if the list includes nvidia in the list
+    (lib.mkIf (builtins.elem "nvidia" cfg.vendor) {
+      boot = {
+        kernelModules = [
+          "nvidia"
+          "nvidia_modeset"
+          "nvidia_uvm"
+          "nvidia_drm"
+        ];
+        kernelParams = [
+          "nvidia-drm.modeset=1"
+        ];
+      };
+      # List packages system hardware configuration
+      hardware = {
+        # GPU (Nvidia)
+        nvidia = {
+          modesetting.enable = true;
+          powerManagement.enable = true;
+          powerManagement.finegrained = false;
+          open = false;
+          nvidiaSettings = true;
+          package = config.boot.kernelPackages.nvidiaPackages.latest;
+        };
+      };
+    })
+
+    # NVIDIA offload support
+    (lib.mkIf (config.hardware.nvidia.prime.offload.enable && cfg.enable) {
+      environment.systemPackages = [ nvidia-offload ];
+    })
+
+    # if the list includes amdgpu in the list
+    (lib.mkIf (builtins.elem "amdgpu" cfg.vendor) {
+      hardware.amdgpu = {
+        initrd.enable = true;
+        opencl.enable = true;
+        zluda.enable = true;
+      };
+    })
+  ]);
+}
