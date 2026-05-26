e42145cbaa671a488881b3e5378dea9374c977e8
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-05-27 01:55:09 +0500

    chore: passing module list for manager

--- modules/nixos/metadata/default.nix
+++ modules/nixos/metadata/default.nix
@@ -5,17 +5,22 @@
 }:
 {
   config = {
-    environment.etc =
-      builtins.mapAttrs
-        (name: path: {
-          source = ../${path}/module.yml;
-        })
-        (
-          lib.filterAttrs (n: v: lib.hasAttr "module.yml" (builtins.readDir ../${v})) (
-            lib.mapAttrs' (name: value: lib.nameValuePair "xinux-modules/${name}/module.yml" name) (
-              builtins.readDir ./..
-            )
-          )
-        );
+    environment.etc = lib.mkMerge [
+      (
+        builtins.readDir ./..
+        |> lib.mapAttrs' (name: value: lib.nameValuePair "xinux-modules/${name}/module.yml" name)
+        |> lib.filterAttrs (n: v: lib.hasAttr "module.yml" (builtins.readDir ../${v}))
+        |> builtins.mapAttrs (name: path: { source = ../${path}/module.yml; })
+      )
+      {
+        "xinux-modules/modules.json" =
+          builtins.readDir ./..
+          |> lib.mapAttrs' (name: value: lib.nameValuePair "xinux-modules/${name}/module.yml" name)
+          |> lib.filterAttrs (n: v: lib.hasAttr "module.yml" (builtins.readDir ../${v}))
+          |> builtins.attrValues
+          |> map (n: "xinux-modules.nixosModules.${n}")
+          |> builtins.toJSON;
+      }
+    ];
   };
 }
