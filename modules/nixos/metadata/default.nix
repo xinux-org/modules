9bc7428f830734cf1dd9aa187908914b9baf3c16
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-05-27 01:57:36 +0500

    fix: define the modules.json is a text

--- modules/nixos/metadata/default.nix
+++ modules/nixos/metadata/default.nix
@@ -13,7 +13,7 @@
         |> builtins.mapAttrs (name: path: { source = ../${path}/module.yml; })
       )
       {
-        "xinux-modules/modules.json" =
+        "xinux-modules/modules.json".text =
           builtins.readDir ./..
           |> lib.mapAttrs' (name: value: lib.nameValuePair "xinux-modules/${name}/module.yml" name)
           |> lib.filterAttrs (n: v: lib.hasAttr "module.yml" (builtins.readDir ../${v}))
