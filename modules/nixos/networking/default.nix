e8406d84446bfbe9f6d59109ac4ae956081636fb
bahrom04 <magdiyevbahrom@gmail.com>
2026-05-27 16:11:50 +0500

    chore(networking): move up add blocker first

--- modules/nixos/networking/default.nix
+++ modules/nixos/networking/default.nix
@@ -9,16 +9,16 @@
       networkmanager.enable = lib.mkDefault true;
       wireless.enable = lib.mkDefault false;
       nameservers = lib.mkDefault [
-        # quad9
-        "9.9.9.9"
-        "149.112.112.112"
-        "2620:fe::fe"
-        "2620:fe::9"
         # AdGuard DNS
         "94.140.14.14"
         "94.140.15.15"
         "2a10:50c0::ad1:ff"
         "2a10:50c0::ad2:ff"
+        # quad9
+        "9.9.9.9"
+        "149.112.112.112"
+        "2620:fe::fe"
+        "2620:fe::9"
       ];
     };
     # Workaround for https://github.com/NixOS/nixpkgs/issues/180175
