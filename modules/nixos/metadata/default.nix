0cfa16a4562ef08f0eff5533ba8924a1b2ca817f
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-05-27 18:41:38 +0500

    chore: remove boot options from modules list

--- modules/nixos/metadata/default.nix
+++ modules/nixos/metadata/default.nix
@@ -18,6 +18,10 @@
           |> lib.mapAttrs' (name: value: lib.nameValuePair "xinux-modules/${name}/module.yml" name)
           |> lib.filterAttrs (n: v: lib.hasAttr "module.yml" (builtins.readDir ../${v}))
           |> builtins.attrValues
+          # Remove boot options as they are appended on user side
+          |> builtins.filter (m: m != "efiboot")
+          |> builtins.filter (m: m != "biosboot")
+          # Convert to parsable module list
           |> map (n: "xinux-modules.nixosModules.${n}")
           |> builtins.toJSON;
       }
