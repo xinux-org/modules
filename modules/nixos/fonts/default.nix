21176961e90dde8745be6d940b6d6667ae6b08f8
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-07-16 15:29:58 +0500

    chore: final touches to concpet

--- modules/nixos/fonts/default.nix
+++ modules/nixos/fonts/default.nix
@@ -10,6 +10,12 @@ let
 in
 {
   options.modules.fonts = {
+    base-fonts = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Foundation fonts for everything.";
+    };
+
     windows-fonts = lib.mkOption {
       type = lib.types.bool;
       default = true;
@@ -36,58 +42,95 @@ in
   };
 
   config = {
-    fonts.packages = builtins.concatLists [
-      (lib.optionals cfg.windows-fonts (
-        with pkgs;
-        [
-          corefonts
-          carlito
-          vista-fonts
-          vista-fonts-chs
-        ]
-      ))
+    fonts = {
+      fontDir.enable = true;
+      enableDefaultPackages = true;
+      packages = builtins.concatLists [
+        (lib.optionals cfg.base-fonts (
+          with pkgs;
+          [
+            # TODO: clean up fonts that aren't necessary
+            ubuntu-classic
+            font-bh-ttf
+            noto-fonts
+            noto-fonts-cjk-sans
+            noto-fonts-cjk-serif
+            nerd-fonts.jetbrains-mono
+            mplus-outline-fonts.osdnRelease
+            fira-code
+            fira-code-symbols
+            hermit
+            source-code-pro
+            terminus_font
+            font-awesome
+            font-awesome_4
+            hack-font
+            noto-fonts
+            # An alternative popular Chinese font
+            wqy_zenhei
+            # cantarell-fonts
+            powerline-fonts
+            roboto
+            roboto-slab
+            montserrat
+            inter
+            lato
+            eb-garamond
+            fira-code
+            fira-code-symbols
+            mplus-outline-fonts.githubRelease
+            dina-font
+            proggyfonts
+            oswald
+            rubik
+            freefont_ttf
+            mononoki
+            iosevka
+          ]
+        ))
 
-      (lib.optionals cfg.apple-fonts (
-        with inputs.apple-fonts.packages;
-        [
-          ny
-          ny-nerd
-          sf-arabic
-          sf-arabic-nerd
-          sf-armenian
-          sf-armenian-nerd
-          sf-compact
-          sf-compact-nerd
-          sf-georgian
-          sf-georgian-nerd
-          sf-hebrew
-          sf-hebrew-nerd
-          sf-mono
-          sf-mono-nerd
-          sf-pro
-          sf-pro-nerd
-        ]
-      ))
+        (lib.optionals cfg.windows-fonts (
+          with pkgs;
+          [
+            corefonts
+            carlito
+            vista-fonts
+            vista-fonts-chs
+          ]
+        ))
 
-      (lib.optionals cfg.nerd-fonts (
-        with pkgs;
-        [
-          corefonts
-          carlito
-          vista-fonts
-          vista-fonts-chs
-        ]
-      ))
+        # Whatever to come out from apple-fonts flake
+        # HINT: evaluate apple-fonts.packages for more.
+        (lib.optionals cfg.apple-fonts (
+          with inputs.apple-fonts.packages;
+          [
+            ny
+            ny-nerd
+            sf-arabic
+            sf-arabic-nerd
+            sf-armenian
+            sf-armenian-nerd
+            sf-compact
+            sf-compact-nerd
+            sf-georgian
+            sf-georgian-nerd
+            sf-hebrew
+            sf-hebrew-nerd
+            sf-mono
+            sf-mono-nerd
+            sf-pro
+            sf-pro-nerd
+          ]
+        ))
 
-      (lib.optionals cfg.designer-picked (
-        with pkgs;
-        [
-          corefonts
-          carlito
-          vista-fonts
-          vista-fonts-chs
-        ]
-      ))
-    ];
+        # Install whole nerd-fonts collection
+        (lib.optionals cfg.nerd-fonts (
+          builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts)
+        ))
+
+        # Fonts chosen by designers
+        (lib.optionals cfg.designer-picked (with pkgs; [ ]))
+      ];
+    };
   };
 }
--- modules/nixos/graphical/desktop.nix
+++ modules/nixos/graphical/desktop.nix
@@ -124,49 +124,6 @@ in
       };
     };
 
-    fonts = {
-      enableDefaultPackages = true;
-      fontDir.enable = true;
-      packages = with pkgs; [
-        # An alternative popular Chinese font
-        wqy_zenhei
-        ubuntu-classic
-        font-bh-ttf
-        noto-fonts
-        noto-fonts-cjk-sans
-        noto-fonts-cjk-serif
-        nerd-fonts.jetbrains-mono
-        mplus-outline-fonts.osdnRelease
-        fira-code
-        fira-code-symbols
-        hermit
-        source-code-pro
-        terminus_font
-        font-awesome
-        font-awesome_4
-        hack-font
-        noto-fonts
-        # cantarell-fonts
-        powerline-fonts
-        roboto
-        roboto-slab
-        montserrat
-        inter
-        lato
-        eb-garamond
-        fira-code
-        fira-code-symbols
-        mplus-outline-fonts.githubRelease
-        dina-font
-        proggyfonts
-        oswald
-        rubik
-        freefont_ttf
-        mononoki
-        iosevka
-      ];
-    };
-
     environment.variables = {
       # Disable compositing mode in WebKitGTK
       # https://github.com/NixOS/nixpkgs/issues/32580
